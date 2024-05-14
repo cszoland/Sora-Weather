d2e916e1e3f50dba448013cb9dec388f1cdfdea7
cszoland <127774589+cszoland@users.noreply.github.com>
2024-05-14 18:22:22 +0900

    Added date and time function

--- src/index.js
+++ src/index.js
@@ -6,13 +6,37 @@ function refreshWeather(response) {
   let descriptionUpper = response.data.condition.description;
   let humidityElement = document.querySelector("#humidity");
   let speedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML =
     descriptionUpper[0].toUpperCase() + descriptionUpper.slice(1);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   speedElement.innerHTML = `${response.data.wind.speed} mph`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
