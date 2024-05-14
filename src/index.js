1d9ebe5afa796c703fac07f2d07396e8eeb535cf
cszoland <127774589+cszoland@users.noreply.github.com>
2024-05-14 21:48:03 +0900

    Added JS for Icon

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ function refreshWeather(response) {
   let speedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
@@ -16,6 +17,7 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   speedElement.innerHTML = `${response.data.wind.speed} mph`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
