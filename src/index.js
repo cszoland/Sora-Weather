093c1932f0ca76185858814585738892586d4c2d
cszoland <127774589+cszoland@users.noreply.github.com>
2024-07-23 08:38:26 -0700

    Updated forecast day with API Info

--- src/index.js
+++ src/index.js
@@ -55,6 +55,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "0ob71b00492eetb34cb4fa4ae8c41bec";
   let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
@@ -62,8 +69,6 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
@@ -72,7 +77,7 @@ function displayForecast(response) {
         forecastHtml +
         `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">Tue</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
         <div>
           <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         </div>
